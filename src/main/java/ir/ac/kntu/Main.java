ee0d01c0cb63c4d63fd1c80f62ef94382aa3049d
shayankoohi <ishayankoo@icloud.com>
2022-01-29 13:44:14 +0330

    added keyboard control for movement, optimized the velocity of the hero with respect to the window height and width.

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     private Pane root = new Pane();
     private Hero player = new Hero(300, 700, 40, 40);
+    private Scene scene = new Scene(contentAdder());
     private Parent contentAdder(){
         root.setPrefSize(600, 800);
         root.getChildren().add(player);
@@ -19,7 +20,34 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setScene(new Scene(contentAdder()));
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()){
+                case A:
+                    player.moveLeft();
+                    break;
+                case D:
+                    player.moveRight();
+                    break;
+                case S:
+                    player.moveDown();
+                    break;
+                case W:
+                    player.moveUp();
+                    break;
+                case SPACE:
+                    player.shoot();
+                    break;
+            }
+                });
+        stage.setScene(scene);
         stage.show();
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
     }
 }
