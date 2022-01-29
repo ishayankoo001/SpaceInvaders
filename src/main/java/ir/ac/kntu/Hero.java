f09af217dd2b492f5b1890cedd0baca28f3fde82
shayankoohi <ishayankoo@icloud.com>
2022-01-29 16:20:48 +0330

    unnecessary for loops removed to increase efficiency, enemies randomly shoot now.

--- src/main/java/ir/ac/kntu/Hero.java
+++ src/main/java/ir/ac/kntu/Hero.java
@@ -1,27 +1,51 @@
 package ir.ac.kntu;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Hero extends Rectangle {
+    Main app;
     private boolean isDead = false;
-    Hero(int x, int y, int w, int h){
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    private int hp;
+
+    Hero(int x, int y, int w, int h, int hp) {
         super(w, h);
         setTranslateX(x);
         setTranslateY(y);
+        this.hp = hp;
     }
-    void moveLeft(){
-        setTranslateX(getTranslateX()-20);
+
+    void moveLeft() {
+        setTranslateX(getTranslateX() - 20);
     }
-    void moveRight(){
-        setTranslateX(getTranslateX()+20);
+
+    void moveRight() {
+        setTranslateX(getTranslateX() + 20);
     }
-    void moveUp(){
-        setTranslateY(getTranslateY()-20);
+
+    void moveUp() {
+        setTranslateY(getTranslateY() - 20);
     }
-    void moveDown(){
-        setTranslateY(getTranslateY()+20);
+
+    void moveDown() {
+        setTranslateY(getTranslateY() + 20);
     }
-    void shoot(){
+
+    void shoot() {
+        Bullet b = new Bullet(this.getTranslateX() + 20, this.getTranslateY(), 5, 20, Color.GOLD);
+        b.sender = "Hero";
+        app.getRoot().getChildren().add(b);
+        app.getBullets().add(b);
+
 
     }
 }
--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,17 +1,90 @@
 package ir.ac.kntu;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Main extends Application {
     private Pane root = new Pane();
-    private Hero player = new Hero(300, 700, 40, 40);
+    private double time = 0;
+    Text text = new Text();
+    private Hero player = new Hero(300, 700, 40, 40, 100);
     private Scene scene = new Scene(contentAdder());
+    private ArrayList<Enemy> enemies = new ArrayList<>();
+    private ArrayList<Bullet> bullets = new ArrayList<>();
     private Parent contentAdder(){
         root.setPrefSize(600, 800);
         root.getChildren().add(player);
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                update();
+
+            }
+        };
+        timer.start();
         return root;
+    }
+    private void update(){
+        root.getChildren().remove(text);
+        text.setText("Your Hero's HP: "+ player.getHp());
+        text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        text.setX(10);
+        text.setY(30);
+        root.getChildren().add(text);
+        time += 0.016;
+            for(Bullet b:bullets){
+                switch (b.sender) {
+                    case "Hero":
+                        b.moveUp();
+                        for (Enemy s : enemies) {
+                                if (b.getBoundsInParent().intersects(s.getBoundsInParent())) {
+                                    root.getChildren().remove(s);
+                                    enemies.remove(s);
+                                    root.getChildren().remove(b);
+                                    bullets.remove(b);
+                                }
+                        }
+                        break;
+                    case "Enemy":
+                        b.moveDown();
+                        if (b.getBoundsInParent().intersects(player.getBoundsInParent())) {
+                                root.getChildren().remove(b);
+                                bullets.remove(b);
+                                player.setHp(player.getHp() - 10);
+                        }
+                        break;
+
+                }
+            }
+            if(player.getHp()<0){
+                System.out.println("Game over");
+                root.getChildren().remove(player);
+            }
+            for(Enemy e:enemies){
+                if(time>2){
+                    if(Math.random()<0.3){
+                        e.shoot();
+                    }
+                }
+            }
+
+        if (time>2){
+            time=0;
+        }
+
     }
     public static void main(String[] args) {
         launch(args);
@@ -20,6 +93,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        getPlayer().app=this;
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()){
                 case A:
@@ -39,6 +113,7 @@ public class Main extends Application {
                     break;
             }
                 });
+        enemyBuilder();
         stage.setScene(scene);
         stage.show();
     }
@@ -46,8 +121,72 @@ public class Main extends Application {
     public Scene getScene() {
         return scene;
     }
+    public void enemyBuilder(){
+        for (int i = 0; i < 5; i++) {
+            Enemy e = new Enemy(90+i*100,150,30,30, Color.RED);
+            e.app=this;
+            root.getChildren().add(e);
+            enemies.add(e);
+        }
+        for (int i = 0; i < 5; i++) {
+            Enemy e = new Enemy(90 + i * 100, 200, 30, 30, Color.RED);
+            e.app = this;
+            root.getChildren().add(e);
+            enemies.add(e);
+        }
+        for (int i = 0; i < 5; i++) {
+            Enemy e = new Enemy(90 + i * 100, 250, 30, 30, Color.RED);
+            e.app = this;
+            root.getChildren().add(e);
+            enemies.add(e);
+        }
+        for (int i = 0; i < 5; i++) {
+            Enemy e = new Enemy(90 + i * 100, 300, 30, 30, Color.RED);
+            e.app = this;
+            root.getChildren().add(e);
+            enemies.add(e);
+        }
+        for (int i = 0; i < 5; i++) {
+            Enemy e = new Enemy(90 + i * 100, 350, 30, 30, Color.RED);
+            e.app = this;
+            root.getChildren().add(e);
+            enemies.add(e);
+        }
+    }
 
     public void setScene(Scene scene) {
         this.scene = scene;
+    }
+
+    public Pane getRoot() {
+        return root;
+    }
+
+    public void setRoot(Pane root) {
+        this.root = root;
+    }
+
+    public Hero getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Hero player) {
+        this.player = player;
+    }
+
+    public ArrayList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void setEnemies(ArrayList<Enemy> enemies) {
+        this.enemies = enemies;
+    }
+
+    public ArrayList<Bullet> getBullets() {
+        return bullets;
+    }
+
+    public void setBullets(ArrayList<Bullet> bullets) {
+        this.bullets = bullets;
     }
 }
