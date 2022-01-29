1122551adeb1e01f0e1191d9a53ca4cc615d2b18
shayankoohi <ishayankoo@icloud.com>
2022-01-29 13:29:51 +0330

    simple rectangular hero added to scene with move functions, keyboard controls not added yet.

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,13 +1,25 @@
 package ir.ac.kntu;
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 public class Main extends Application {
+    private Pane root = new Pane();
+    private Hero player = new Hero(300, 700, 40, 40);
+    private Parent contentAdder(){
+        root.setPrefSize(600, 800);
+        root.getChildren().add(player);
+        return root;
+    }
     public static void main(String[] args) {
+        launch(args);
         System.out.println("hi");
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
+        stage.setScene(new Scene(contentAdder()));
         stage.show();
     }
 }
